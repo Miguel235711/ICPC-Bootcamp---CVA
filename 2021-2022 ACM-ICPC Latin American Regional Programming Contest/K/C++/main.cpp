d18a1ce033745dd08059f593c2a542af9e4b5feb
Miguel235711 <42991504+Miguel235711@users.noreply.github.com>
2022-06-23 00:12:22 -0500

    Mock up Solution for problem K
    https://codeforces.com/gym/103640/problem/K

---
+++ 2021-2022 ACM-ICPC Latin American Regional Programming Contest/K/C++/main.cpp
@@ -0,0 +1,112 @@
+#include<bits/stdc++.h>
+
+#define mp make_pair 
+#define pb push_back 
+#define MAX(a, b) a = max(a, b) 
+#define MIN(a, b) a = min(a, b) 
+#define SQR(x) ((LL)(x) * (x)) 
+#define RESET(a, b) memset(a, b, sizeof(a))
+#define ALL(v) v.begin(), v.end() 
+#define SORT(v) sort(ALL(v)) 
+#define PERMUTE next_permutation
+#define fi first 
+#define se second
+
+#define MOD 1000000009 //1E9+9
+
+using namespace std;
+
+typedef int64_t i64;
+
+typedef pair<i64,i64> p64;
+typedef vector<i64> v64;
+typedef vector<p64>  v6464;
+typedef vector < v64 > vv64;
+
+typedef double dle;
+typedef long double ldle;
+
+typedef pair<int,int> pii; 
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<vb> vvb;
+typedef vector< pii > vii;
+typedef vector< vi > vvi;
+
+
+typedef pair<string,string> pss;
+typedef vector<string> vs;
+typedef vector<pss> vss;
+
+typedef pair<int,string> pis;
+typedef vector<pis> vis;
+typedef pair<string,int> psi;
+typedef vector<psi> vsi;
+
+typedef map<string,string> mss;
+typedef map<int,string> mis;
+typedef map<string,int> msi;
+
+///operators overloading
+void operator+=(pii& a,const pii&b){
+    a.fi+=b.fi;
+    a.se+=b.se;
+}
+pii operator+(const pii& a,const pii&b){
+    return {a.fi+b.fi,a.se+b.se};
+}
+
+void operator-=(pii& a,const pii&b){
+    a.fi-=b.fi;
+    a.se-=b.se;
+}
+pii operator-(const pii& a,const pii&b){
+    return {a.fi-b.fi,a.se-b.se};
+}
+
+bool is_possible(unordered_set<char> & W,vs & S){
+    for(auto & Si:S){
+        bool is_candidate = true;
+        for(auto c: Si){
+            if(W.find(c)==W.end()){
+                is_candidate = false;
+                break;
+            }
+        }
+        if(is_candidate)
+            return true;
+    }
+    return false;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    unordered_set<char> W;
+    int N;
+    cin >> N;
+    vs S(N);
+    for(int i = 0 ; i < N ; i ++){
+        cin >> S[i];
+        W.insert(S[i][0]);
+    }
+    //here old solution would go...
+    //to_string(3);
+    cout << (is_possible(W,S) ? 'Y' : 'N') << '\n';
+    return 0;
+}
+
+//old solution:
+ // char ans = 'N';
+    // for(auto & Si:S){
+    //     bool is_candidate = true;
+    //     for(auto c: Si){
+    //         if(W.find(c)==W.end()){
+    //             is_candidate = false;
+    //             break;
+    //         }
+    //     }
+    //     if(is_candidate){
+    //         ans = 'Y';
+    //         break;
+    //     }
+    // }
